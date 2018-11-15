91b2c551c11d1d74fcb061e5e77236dc02ef1f20
Morgan McCrory <mmccrory@5672.fios-router.home>
2018-11-14 21:24:10 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './codeshewrote.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+          Coming soon.
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
